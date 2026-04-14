4b4692c0034c3b2d703153833e03e1306bc7d47a
ilyasaitbamou <ilyasaitbamou2005@gmail.com>
2026-04-14 13:13:32 +0100

    setup database design & connection

---
+++ src/Database.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Src;
+
+use PDO;
+use PDOException;
+
+class Database {
+    protected $db;
+
+    public function __construct() {
+        $config = require dirname(__DIR__) . '/config/database.php';
+
+        $dsn = sprintf(
+            '%s:host=%s;dbname=%s;charset=%s;port=%d',
+            $config['driver'],
+            $config['host'],
+            $config['dbname'],
+            $config['charset'],
+            $config['port']
+        );
+
+        try {
+            $this->db = new PDO($dsn, $config['username'], $config['password']);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die('Connection failed: ' . $e->getMessage());
+        }
+    }
+}
